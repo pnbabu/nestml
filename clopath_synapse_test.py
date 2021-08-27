4ebace7c1bf901f33c0993d019fc607378f777bc
Pooja Babu <p.babu@fz.juelich.de>
2021-08-27 15:05:05 +0200

    Modify the nestml and test files

--- clopath_synapse_test.py
+++ clopath_synapse_test.py
@@ -59,7 +59,7 @@ to_nest(input_path=["models/iaf_psc_delta.nestml", "models/clopath_synapse.nestm
                       "neuron_parent_class_include": "structural_plasticity_node.h",
                       "neuron_synapse_pairs": [{"neuron": "iaf_psc_delta",
                                                 "synapse": "clopath",
-                                                "post_ports": ["post_spikes", "v_clamp"]}]})
+                                                "post_ports": ["post_spikes", ["v_clamp", "V_abs"]]}]})
 install_nest("/tmp/nestml-clopath", nest_install_path)
 
 neuron_model_name = "iaf_psc_delta_nestml__with_clopath_nestml"
