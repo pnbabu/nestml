692f47009f066f0c77b65ad144899da1cd8ac82a
Pooja Babu <p.babu@fz-juelich.de>
2026-09-07 10:58:17 +0200

    Remove comments

--- pynestml/codegeneration/nest_code_generator.py
+++ pynestml/codegeneration/nest_code_generator.py
@@ -596,8 +596,6 @@ class NESTCodeGenerator(CodeGenerator):
             # separate (not neuron+synapse co-generated)
             namespace["pre_ports"] = all_input_port_names
 
-        # assert len(namespace["pre_ports"]) <= 1, "Synapses only support one spiking input port"
-
         namespace["synapseName"] = synapse.get_name()
         namespace["synapse"] = synapse
         namespace["moduleName"] = FrontendConfiguration.get_module_name()
